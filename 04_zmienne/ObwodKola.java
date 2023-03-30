25918193d839b72be6336e23a97af023a695e7ba
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2023-03-30 15:14:40 +0200

    Dodanie rozwiązania zadania #4 - Obwód koła

---
+++ 04_zmienne/ObwodKola.java
@@ -0,0 +1,14 @@
+public class ObwodKola {
+  public static void main(String[] args) {
+    final double PI = 3.14;
+    double r = 4.6;
+
+    double obwod = 2 * PI * r;
+
+    System.out.printf(
+        "Obwód koła o promieniu %.2f wynosi %.2f.%n",
+        r,
+        obwod
+    );
+  }
+}

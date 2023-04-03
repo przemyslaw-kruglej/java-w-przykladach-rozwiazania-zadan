577ee70d76c2c25ff96b5c7e5add3e13bab598f4
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2023-04-03 20:19:26 +0200

    Dodanie rozwiązania zadania #28 - Przeciwne wartości

---
+++ 07_tablice/PrzeciwneWartosci.java
@@ -0,0 +1,9 @@
+public class PrzeciwneWartosci {
+  public static void main(String[] args) {
+    int[] tablica = { 4, 0, -20, 100, -1 };
+
+    for (int liczba : tablica) {
+      System.out.println(-liczba);
+    }
+  }
+}

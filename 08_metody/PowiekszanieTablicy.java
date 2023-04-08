aa8ec8dbe8ad1797e7eeaca0a092b36c88425c70
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2023-04-08 16:26:22 +0200

    Dodanie rozwiązania zadania #44 - Powiększanie tablicy

---
+++ 08_metody/PowiekszanieTablicy.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class PowiekszanieTablicy {
+  public static void main(String[] args) {
+    int[] liczby = { 30, -5, 121 };
+
+    int[] powiekszona = powiekszDwukrotnie(liczby);
+    int[] powiekszonaDomyslnaWartosc = powiekszDwukrotnie(liczby, 5);
+
+    System.out.println(Arrays.toString(powiekszona));
+    System.out.println(Arrays.toString(powiekszonaDomyslnaWartosc));
+  }
+
+  public static int[] powiekszDwukrotnie(int[] tab) {
+    return Arrays.copyOf(tab, tab.length * 2);
+  }
+
+  public static int[] powiekszDwukrotnie(int[] tab, int domyslnaWartosc) {
+    int[] powiekszonaTablica = powiekszDwukrotnie(tab);
+    Arrays.fill(
+        powiekszonaTablica,
+        tab.length,
+        powiekszonaTablica.length,
+        domyslnaWartosc
+    );
+    return powiekszonaTablica;
+  }
+}

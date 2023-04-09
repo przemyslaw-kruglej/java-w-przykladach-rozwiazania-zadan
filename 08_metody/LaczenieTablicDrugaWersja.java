c073700b052ff8e583f78a2ceea0eecced89260a
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2023-04-09 12:03:29 +0200

    Dodanie rozwiązania zadania #45 - Usuwanie elementu tablicy

---
+++ 08_metody/LaczenieTablicDrugaWersja.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class LaczenieTablicDrugaWersja {
+  public static void main(String[] args) {
+    System.out.println(
+        Arrays.toString(usunElement(new int[] { 1 }, 0))
+    );
+    System.out.println(
+        Arrays.toString(usunElement(new int[] { 25, 100, -5 }, 1))
+    );
+    System.out.println(
+        Arrays.toString(usunElement(new int[] { 25, 100, -5 }, 3))
+    );
+  }
+
+  public static int[] usunElement(int[] liczby, int indeks) {
+    if (indeks < 0 || indeks >= liczby.length) {
+      throw new IllegalArgumentException(
+          "Indeks wykracza poza zakres tablicy."
+      );
+    }
+
+    int[] rezultat = new int[liczby.length - 1];
+
+    for (int i = 0; i < rezultat.length; i++) {
+      if (i < indeks) {
+        rezultat[i] = liczby[i];
+      } else {
+        rezultat[i] = liczby[i + 1];
+      }
+    }
+    return rezultat;
+  }
+}

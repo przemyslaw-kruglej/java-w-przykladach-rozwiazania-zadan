7ed486f5bbbda7d6bea7b03b0596a09593f033bf
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2023-04-10 20:20:23 +0200

    Dodanie rozwiązania zadania #48 - Argumenty zapisane w Camel Case

---
+++ 08_metody/ZapiszArgumentyWCamelCase.java
@@ -0,0 +1,26 @@
+public class ZapiszArgumentyWCamelCase {
+  public static void main(String[] args) {
+    System.out.println(doCamelCase(args));
+  }
+
+  public static String doCamelCase(String[] tab) {
+    StringBuilder sb = new StringBuilder();
+
+    for (String s : tab) {
+      String strBezBialychZnakow = s.replaceAll("\\s", "");
+
+      if (strBezBialychZnakow.isEmpty()) {
+        continue;
+      }
+
+      if (sb.isEmpty()) {
+        sb.append(strBezBialychZnakow.toLowerCase());
+      } else {
+        sb.append(Character.toUpperCase(strBezBialychZnakow.charAt(0)));
+        sb.append(strBezBialychZnakow.substring(1).toLowerCase());
+      }
+    }
+
+    return sb.toString();
+  }
+}

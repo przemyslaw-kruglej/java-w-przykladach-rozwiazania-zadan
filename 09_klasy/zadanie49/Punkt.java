39d8aa922678620901140a5b12dfcf7b1dd380c8
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2023-04-11 19:29:56 +0200

    Dodanie rozwiązań do zadania #49 - Klasa Punkt

---
+++ 09_klasy/zadanie49/Punkt.java
@@ -0,0 +1,48 @@
+package zadanie49;
+
+import java.util.Objects;
+
+public class Punkt {
+  private int x, y;
+
+  public Punkt(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public void setX(int x) {
+    this.x = x;
+  }
+
+  public void setY(int y) {
+    this.y = y;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("Punkt(x:%d, y:%d)", x, y);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Punkt punkt = (Punkt) o;
+    return x == punkt.x && y == punkt.y;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(x, y);
+  }
+}

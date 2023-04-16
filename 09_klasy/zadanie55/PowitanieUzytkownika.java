3ff9a882c0e7ffe7ea32821ab73e62d67a1d574f
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2023-04-16 12:25:27 +0200

    Dodanie rozwiązania zadania #55 - JAR z powitaniem użytkownika

---
+++ 09_klasy/zadanie55/PowitanieUzytkownika.java
@@ -0,0 +1,17 @@
+package zadanie55;
+
+import java.util.Scanner;
+
+public class PowitanieUzytkownika {
+  public static void main(String[] args) {
+    System.out.print("Podaj imię: ");
+
+    String imie = pobierzSlowo();
+
+    System.out.printf("Witaj %s", imie);
+  }
+
+  public static String pobierzSlowo() {
+    return new Scanner(System.in).next();
+  }
+}
